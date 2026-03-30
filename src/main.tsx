55c055139a372479dc24e748f2883e2842f0b8f8
sokodigital <isaacmutinda752@gmail.com>
2026-03-30 21:19:35 +0300

    Configure GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/WebrayzDev/">
+    <BrowserRouter basename="/sokodigital/">
       <App />
     </BrowserRouter>
   </StrictMode>,
